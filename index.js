4f30a24d09cf0de75fc037db7d91bb5caa932cf8
Abin John <abinjohnanto@gmail.com>
2022-03-28 22:56:40 +0530

    develop regex to extract timestamps from input text

--- index.js
+++ index.js
@@ -16,6 +16,35 @@ Flow
 (15:24) Section 5
 (20:22) - Section 6
 (45:51): Section 7
+
+
+RegEx
+\d{0,2}:{0,1}\d{1,2}:\d{1,2}
+
+
+
+// const times = [
+//   '03:30',
+//   '09:34',
+//   '14:53',
+//   '17:32',
+//   '20:55',
+//   '24:07',
+//   '27:48',
+//   '29:10',
+//   '33:36',
+//   '38:57',
+//   '42:44',
+//   '46:44',
+//   '50:23',
+//   '01:00:57',
+//   '01:02:01',
+//   '01:05:16',
+//   '01:11:18',
+//   '01:13:27',
+//   '01:17:25',
+// ];
+
 */
 
 const inputTextEl = document.getElementById('input-text');
@@ -23,14 +52,16 @@ const timeAdjustEl = document.getElementById('time-to-adjust');
 const buttonEl = document.getElementById('btn-adjust');
 
 const loadSampleInput = function () {
-  inputTextEl.value = `02:30 - Section 1
+  inputTextEl.value = `01:10 - Section 0
+02:30 - Section 1
 05:35 - Section 2
 07:53 - Section 3
 10:25 - Section 4
 (15:24) Section 5
 (20:22) - Section 6
-(45:51): Section 7
-`;
+(45:51): Section 7`;
+
+  timeAdjustEl.value = '2:08';
 };
 
 const timeToSeconds = (time) => {
@@ -61,38 +92,24 @@ const secondsToTime = (sec) => {
     : hours + ':' + minutes + ':' + seconds; // Return in HH:MM:SS
 };
 
-const timeToSubtract = timeToSeconds('02:08');
+const reduceTime = function (text, timeToSubtract) {
+  const timestamps = text
+    .split('\n')
+    .map((line) => line.match(/\d{0,2}:{0,1}\d{1,2}:\d{1,2}/)[0]);
 
-// const times = [
-//   '03:30',
-//   '09:34',
-//   '14:53',
-//   '17:32',
-//   '20:55',
-//   '24:07',
-//   '27:48',
-//   '29:10',
-//   '33:36',
-//   '38:57',
-//   '42:44',
-//   '46:44',
-//   '50:23',
-//   '01:00:57',
-//   '01:02:01',
-//   '01:05:16',
-//   '01:11:18',
-//   '01:13:27',
-//   '01:17:25',
-// ];
+  const updatedTimestamps = timestamps.map((time) =>
+    secondsToTime(timeToSeconds(time) - timeToSubtract)
+  );
 
-// times.forEach((time) => {
-//   const reduced = secondsToTime(timeToSeconds(time) - timeToSubtract);
-// });
-
-loadSampleInput();
+  for (let i = 0; i < timestamps.length; ++i)
+    console.log(timestamps[i], updatedTimestamps[i]);
+};
 
 buttonEl.addEventListener('click', function (e) {
   e.preventDefault();
   const inputText = inputTextEl.value;
-  console.log(inputText);
+  const timeToSubtract = timeToSeconds(timeAdjustEl.value);
+  reduceTime(inputText, timeToSubtract);
 });
+
+loadSampleInput();
